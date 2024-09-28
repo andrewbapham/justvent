cda689f279cd3400717c40f4ecec0e86e96c2c19
Noah do Rego <ndore051@uottawa.ca>
2024-09-28 01:08:38 -0400

    Set up navbar functionality

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,9 +1,37 @@
-import { NavLink } from 'react-router-dom'
+import { AppShell, Burger, Group, UnstyledButton } from "@mantine/core";
+import { useNavigate } from "react-router-dom";
 
 function NavBar() {
-    return (
-        <h1>NavBar</h1>
-    )
+  const navigate = useNavigate();
+  return (
+    <AppShell.Header>
+      <Group h="100%" px="md">
+        <Group justify="justify-between" style={{ flex: 1, padding: "20px" }}>
+          <Group ml="xl" gap={10} visibleFrom="sm">
+            <UnstyledButton
+              onClick={() => navigate("/")}
+              style={{ padding: "8px" }}
+            >
+              Home
+            </UnstyledButton>
+            <UnstyledButton
+              onClick={() => navigate("/journal")}
+              style={{ padding: "8px" }}
+            >
+              Journal
+            </UnstyledButton>
+            <UnstyledButton
+              onClick={() => navigate("/dashboard")}
+              style={{ padding: "8px" }}
+            >
+              Dashboard
+            </UnstyledButton>
+            <UnstyledButton style={{ padding: "8px" }}>Sign In</UnstyledButton>
+          </Group>
+        </Group>
+      </Group>
+    </AppShell.Header>
+  );
 }
 
 export default NavBar;
