b0e9d8ffca98a0707fcaedaaa0d4cb1871565163
Noah do Rego <ndore051@uottawa.ca>
2024-09-28 02:21:06 -0400

    Awesome home page

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,33 +4,31 @@ import { useNavigate } from "react-router-dom";
 function NavBar() {
   const navigate = useNavigate();
   return (
-    <AppShell.Header>
-      <Group h="100%" px="md">
-        <Group justify="justify-between" style={{ flex: 1, padding: "20px" }}>
-          <Group ml="xl" gap={10} visibleFrom="sm">
-            <UnstyledButton
-              onClick={() => navigate("/")}
-              style={{ padding: "8px" }}
-            >
-              Home
-            </UnstyledButton>
-            <UnstyledButton
-              onClick={() => navigate("/journal")}
-              style={{ padding: "8px" }}
-            >
-              Journal
-            </UnstyledButton>
-            <UnstyledButton
-              onClick={() => navigate("/dashboard")}
-              style={{ padding: "8px" }}
-            >
-              Dashboard
-            </UnstyledButton>
-            <UnstyledButton style={{ padding: "8px" }}>Sign In</UnstyledButton>
-          </Group>
+    <Group h="100%" px="md">
+      <Group justify="justify-between" style={{ flex: 1, padding: "20px" }}>
+        <Group ml="xl" gap={10} visibleFrom="sm">
+          <UnstyledButton
+            onClick={() => navigate("/")}
+            style={{ padding: "8px" }}
+          >
+            Home
+          </UnstyledButton>
+          <UnstyledButton
+            onClick={() => navigate("/journal")}
+            style={{ padding: "8px" }}
+          >
+            Journal
+          </UnstyledButton>
+          <UnstyledButton
+            onClick={() => navigate("/dashboard")}
+            style={{ padding: "8px" }}
+          >
+            Dashboard
+          </UnstyledButton>
+          <UnstyledButton style={{ padding: "8px" }}>Sign In</UnstyledButton>
         </Group>
       </Group>
-    </AppShell.Header>
+    </Group>
   );
 }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,13 @@
+import { Center, Box, Title } from "@mantine/core";
+
 function Home() {
-    return (
-        <h1>Home</h1>
-    )
+  return (
+    <Center bg="#fefae0" w={"100vw"} h={"100vh"}>
+      <Box bg="#e9edc9" p="lg">
+        <Title order={1}>Feel heard using AI</Title>
+      </Box>
+    </Center>
+  );
 }
 
 export default Home;
