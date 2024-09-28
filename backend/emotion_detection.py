523d6302991cd45e547c156dd63d7b41ea9d75e9
Colby Todd <ctodd083@uottawa.ca>
2024-09-28 11:58:16 -0400

    Refactoring

--- backend/emotion_detection.py
+++ backend/emotion_detection.py
@@ -2,6 +2,7 @@ from transformers import pipeline
 import pandas as pd
 import plotly.express as px
 import re
+from datetime import datetime, timezone, timedelta
 
 class EmotionDetection:
 
@@ -9,37 +10,57 @@ class EmotionDetection:
         # download pre-trained emotion classification model
         self.model = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base") # Pytorch 
         self.df = pd.DataFrame() # Pandas Dataframe
-    
+
     def translateText(self, text: str) -> None:
+        # Returns None.
+        # This will split the text by sentence and put it into a 
+        # pandas dataframe.
         text = re.split('([.!?])', string)
         dataset = {"text": [x+y for x,y in zip(text[0::2], text[1::2])]}
         self.df = pd.DataFrame(dataset)
-    
-    def detectEmotions(self, text) -> None:
+
+    def detectEmotions(self, text: str) -> None:
+        # Returns None.
+        # This uses the model to detect the emotions from the text.
         self.translateText(text)
         # compute the emotion of each tweet using the model
         all_texts = self.df["text"].values.tolist()
         all_emotions = self.model(all_texts)
         self.df["emotion_label"] = [d["label"] for d in all_emotions]
         self.df["emotion_score"] = [d["score"] for d in all_emotions]
-    
-    def getEmotions(self) -> list[dict[int]] | None:
+
+    def getEmotions(self, text: str=None) -> list[dict[int]] | None:
+        # This returns the emotions stored in the dataframe
+        # if the dataframe is empty it will return None.
+        if text:
+            self.detectEmotions()
+
         if self.df.empty:
             return None
-        
-        percentage_dist = self.df["emotion_label"].value_counts(normalize=True) * 100
+
+        percentage_dist = self.df["emotion_label"].value_counts(normalize=True)
         return {emotion: percentage for emotion, percentage in percentage_dist.items()}
-    
+
     def plotEmotions(self) -> None:
-        # plot emotions found in tweets
+        # Plots the emotions detected by the model.
+        if self.df.empty:
+            return None
+
         plot_title = f"Emotions found in tweets"
         fig = px.histogram(self.df, x="emotion_label", template="plotly_dark",
                         title=plot_title, color="emotion_label")
         fig.update_layout(showlegend=False)
         fig.show()
 
-detector = EmotionDetection()
-#"Today started off a little slow, but I managed to make it productive. I woke up later than I wanted to, but I decided not to let it affect my mood. After breakfast, I got to work on the tasks I set for myself the night before. I had a couple of meetings that went smoothly. My afternoon was focused on a project I've been chipping away at for a few weeks. It’s finally starting to come together, and that gave me a sense of accomplishment."
-string = "I am sad! Today started off a little slow, but I managed to make it productive. I woke up later than I wanted to, but I decided not to let it affect my mood? After breakfast, I got to work on the tasks I set for myself the night before. I had a couple of meetings that went smoothly. My afternoon was focused on a project I've been chipping away at for a few weeks. It’s finally starting to come together, and that gave me a sense of accomplishment."
-detector.detectEmotions(string)
-print(detector.getEmotions())
+# detector = EmotionDetection()
+# #"Today started off a little slow, but I managed to make it productive. I woke up later than I wanted to, but I decided not to let it affect my mood. After breakfast, I got to work on the tasks I set for myself the night before. I had a couple of meetings that went smoothly. My afternoon was focused on a project I've been chipping away at for a few weeks. It’s finally starting to come together, and that gave me a sense of accomplishment."
+# string = "I am sad! Today started off a little slow, but I managed to make it productive. I woke up later than I wanted to, but I decided not to let it affect my mood? After breakfast, I got to work on the tasks I set for myself the night before. I had a couple of meetings that went smoothly. My afternoon was focused on a project I've been chipping away at for a few weeks. It’s finally starting to come together, and that gave me a sense of accomplishment."
+# detector.detectEmotions(string)
+# post = {
+#         "user_id": "user_006",
+#         "content": string,
+#         "emotions": detector.getEmotions(),
+#         "date": str(datetime.now(timezone.utc) + timedelta(days=0))
+#     }
+# print(post)
+# detector.plotEmotions()
