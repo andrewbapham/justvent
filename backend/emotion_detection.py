d91bb300ad84804fd1a5a51343127ca86343b4bc
Colby Todd <ctodd083@uottawa.ca>
2024-09-28 02:22:15 -0400

    Added ability to return emotions percentage distribution

--- backend/emotion_detection.py
+++ backend/emotion_detection.py
@@ -7,20 +7,28 @@ class EmotionDetection:
 
     def __init__(self):
         # download pre-trained emotion classification model
-        self.model = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", top_k=None) # Pytorch 
-        self.df = None # Pandas Dataframe
+        self.model = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base") # Pytorch 
+        self.df = pd.DataFrame() # Pandas Dataframe
     
     def translateText(self, text: str) -> None:
         text = re.split('([.!?])', string)
-        dataset = {"text": [x+y for x,y in zip(string[0::2], string[1::2])]}
+        dataset = {"text": [x+y for x,y in zip(text[0::2], text[1::2])]}
         self.df = pd.DataFrame(dataset)
     
-    def detectEmotions(self) -> None:
+    def detectEmotions(self, text) -> None:
+        self.translateText(text)
         # compute the emotion of each tweet using the model
-        all_texts = df["text"].values.tolist()
+        all_texts = self.df["text"].values.tolist()
         all_emotions = self.model(all_texts)
         self.df["emotion_label"] = [d["label"] for d in all_emotions]
         self.df["emotion_score"] = [d["score"] for d in all_emotions]
+    
+    def getEmotions(self) -> list[dict[int]] | None:
+        if self.df.empty:
+            return None
+        
+        percentage_dist = self.df["emotion_label"].value_counts(normalize=True) * 100
+        return {emotion: percentage for emotion, percentage in percentage_dist.items()}
     
     def plotEmotions(self) -> None:
         # plot emotions found in tweets
@@ -29,8 +37,9 @@ class EmotionDetection:
                         title=plot_title, color="emotion_label")
         fig.update_layout(showlegend=False)
         fig.show()
-    
 
-
+detector = EmotionDetection()
 #"Today started off a little slow, but I managed to make it productive. I woke up later than I wanted to, but I decided not to let it affect my mood. After breakfast, I got to work on the tasks I set for myself the night before. I had a couple of meetings that went smoothly. My afternoon was focused on a project I've been chipping away at for a few weeks. It’s finally starting to come together, and that gave me a sense of accomplishment."
 string = "I am sad! Today started off a little slow, but I managed to make it productive. I woke up later than I wanted to, but I decided not to let it affect my mood? After breakfast, I got to work on the tasks I set for myself the night before. I had a couple of meetings that went smoothly. My afternoon was focused on a project I've been chipping away at for a few weeks. It’s finally starting to come together, and that gave me a sense of accomplishment."
+detector.detectEmotions(string)
+print(detector.getEmotions())
