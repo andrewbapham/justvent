8f723be33023229d83890db7b1e988254c82cf2b
Colby Todd <ctodd083@uottawa.ca>
2024-09-28 01:09:34 -0400

    Fixed split removing punctuation marks

--- backend/emotion_detection.py
+++ backend/emotion_detection.py
@@ -4,12 +4,13 @@ import plotly.express as px
 import re
 
 #"Today started off a little slow, but I managed to make it productive. I woke up later than I wanted to, but I decided not to let it affect my mood. After breakfast, I got to work on the tasks I set for myself the night before. I had a couple of meetings that went smoothly. My afternoon was focused on a project I've been chipping away at for a few weeks. It’s finally starting to come together, and that gave me a sense of accomplishment."
-string = "Today started off a little slow, but I managed to make it productive! I woke up later than I wanted to, but I decided not to let it affect my mood? After breakfast, I got to work on the tasks I set for myself the night before. I had a couple of meetings that went smoothly. My afternoon was focused on a project I've been chipping away at for a few weeks. It’s finally starting to come together, and that gave me a sense of accomplishment."
-dataset = {"text": re.split('[.!?]', string)}
+string = "I am sad! Today started off a little slow, but I managed to make it productive. I woke up later than I wanted to, but I decided not to let it affect my mood? After breakfast, I got to work on the tasks I set for myself the night before. I had a couple of meetings that went smoothly. My afternoon was focused on a project I've been chipping away at for a few weeks. It’s finally starting to come together, and that gave me a sense of accomplishment."
+string = re.split('([.!?])', string)
+dataset = {"text": [x+y for x,y in zip(string[0::2], string[1::2])]}
 df = pd.DataFrame(dataset)
 
 # download pre-trained emotion classification model
-model = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base")
+model = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", top_k=None)
 
 # compute the emotion of each tweet using the model
 all_texts = df["text"].values.tolist()
