b1244055bac30dbd7c64ac585b78b7e77698a053
Andrew Pham <andrewbapham@outlook.com>
2024-09-28 16:25:29 -0400

    Fixed database url and added dockerfile

--- backend/database.py
+++ backend/database.py
@@ -10,7 +10,8 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 def get_database_client() -> MongoClient:
 
     # Provide the mongodb atlas url to connect python to mongodb using pymongo
-    CONNECTION_STRING = f"mongodb://justventdb:{DB_PASSWORD}@docdb-2024-09-28-02-58-29.cluster-chwgmeickfoa.us-east-2.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+    CONNECTION_STRING = f"mongodb://justventdb:{
+        DB_PASSWORD}@justvent-docdb2.cluster-chwgmeickfoa.us-east-2.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
     # CONNECTION_STRING = "mongodb://localhost:27017/"
     # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
     client = MongoClient(CONNECTION_STRING)
