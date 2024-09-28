52693708fd36b20548ae7eec8b2b45e7c9faec1c
Noah do Rego <ndore051@uottawa.ca>
2024-09-28 11:17:00 -0400

    Fixed navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,80 +1,72 @@
 import { Group, UnstyledButton } from "@mantine/core";
 import { useNavigate } from "react-router-dom";
-import homeIcon from "../assets/new_make_logo_updated.png"; 
-import './Navbar.css';
+import homeIcon from "../assets/new_make_logo_updated.png";
+import "./Navbar.css";
 
 function NavBar() {
   const navigate = useNavigate();
-  
+
   return (
     <Group
-      h="100%"
+      w="100%"
       px="md"
+      justify="space-between"
       style={{
         borderBottom: "2px solid #e0e0e0",
         paddingBottom: "10px",
       }}
     >
-      <Group justify="space-between" style={{ flex: 1, padding: "10px" }}>
-        <Group ml="xl" gap={2} visibleFrom="sm">
-        
-          <UnstyledButton
-            onClick={() => navigate("/")}
-            className="custom-button"  
-            style={{
-              padding: "8px",
-              outline: "none", 
-              backgroundColor: "transparent", 
-              border: "none", 
-            }}
-          >
-            <img 
-              src={homeIcon}
-              alt="Home"
-              width={80}
-              height={80}
-            />
-          </UnstyledButton>
+      <Group gap={2}>
+        <UnstyledButton
+          onClick={() => navigate("/")}
+          className="custom-button"
+          style={{
+            outline: "none",
+            backgroundColor: "transparent",
+            border: "none",
+            paddingTop: "10px",
+          }}
+        >
+          <img src={homeIcon} alt="Home" width={50} height={50} />
+        </UnstyledButton>
 
-          <UnstyledButton
-            onClick={() => navigate("/journal")}
-            className="custom-button"  
-            style={{
-              padding: "8px",
-              outline: "none",
-              backgroundColor: "transparent",
-              border: "none",
-            }}
-          >
-            Journal
-          </UnstyledButton>
+        <UnstyledButton
+          onClick={() => navigate("/journal")}
+          className="custom-button"
+          style={{
+            padding: "8px",
+            outline: "none",
+            backgroundColor: "transparent",
+            border: "none",
+          }}
+        >
+          Journal
+        </UnstyledButton>
 
-          <UnstyledButton
-            onClick={() => navigate("/dashboard")}
-            className="custom-button" 
-            style={{
-              padding: "8px",
-              outline: "none",
-              backgroundColor: "transparent",
-              border: "none",
-            }}
-          >
-            Dashboard
-          </UnstyledButton>
-
-          <UnstyledButton
-            className="custom-button"  
-            style={{
-              padding: "8px",
-              outline: "none",
-              backgroundColor: "transparent",
-              border: "none",
-            }}
-          >
-            Sign In
-          </UnstyledButton>
-        </Group>
+        <UnstyledButton
+          onClick={() => navigate("/dashboard")}
+          className="custom-button"
+          style={{
+            padding: "8px",
+            outline: "none",
+            backgroundColor: "transparent",
+            border: "none",
+          }}
+        >
+          Dashboard
+        </UnstyledButton>
       </Group>
+      <UnstyledButton
+        className="custom-button"
+        style={{
+          padding: "8px",
+          outline: "none",
+          backgroundColor: "transparent",
+          border: "none",
+        }}
+      >
+        Sign In
+      </UnstyledButton>
     </Group>
   );
 }
