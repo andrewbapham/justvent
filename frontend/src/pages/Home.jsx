27387660a64db8815e7363948ff0e584c6a21fd0
Asvo777 <nmarc068@uottawa.ca>
2024-09-28 14:51:32 -0400

    Signin animation homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,25 @@
 import { Center, Box, Title, Image, Button, Flex } from "@mantine/core";
-import new_make_logo from "../assets/new_make_logo_updated.png"; 
+import new_make_logo from "../assets/new_make_logo_updated.png";
+import notebook from "../assets/notebook.svg";
+import { useState } from "react";
 
 function Home() {
   const spinAnimation = {
     animation: "spin 10s linear infinite",
   };
 
+  const [btnOpacity, setbtnOpacity] = useState(0)
+
+  const [animation, setAnimation] = useState({
+    opacity: 0,
+    width: "200px",
+    position: "absolute",
+    left: "-50px", // Initially off-screen to the left
+    transition: "opacity 2s, left 2s", // Transition for smooth animation
+  });
+
   return (
-    <Center bg="#fefae0" w={"100vw"} h={"100vh"} className="hide-scroll-bar">
+    <Center bg="#fefae0" w={"100vw"} h={"180vh"} className="hide-scroll-bar">
       <section
         style={{
           position: "relative",
@@ -18,7 +30,7 @@ function Home() {
           paddingTop: "2em", // Add some padding to control vertical positioning
         }}
       >
-        <Flex direction="row" align="center" justify="center" p="lg">
+        <Flex direction="row" align="center" justify="center" p="lg" mb="lg">
           {/* Text and Button in a column */}
           <Box display="flex" flexDirection="column" alignItems="flex-start" mr="xl">
             {/* Tagline */}
@@ -40,9 +52,42 @@ function Home() {
             alt="Logo"
             width={160} // Adjust the width as needed
             style={spinAnimation} // Apply spin animation
+
           />
         </Flex>
+       
+        <Flex direction="row-reverse" align="center" justify="space-between" p="3em" mt="10em" w={"80%"} onMouseOver={
+          () => {setAnimation({ ...animation, opacity: 1, left: "180px", width: "350px" });
+           setbtnOpacity(1);
+          }}>
+          {/* Text and Button in a column */}
+          
+          {/* Image with fade animation in intro */}
+          <Image
+            src={notebook} // Use your imported image
+            alt="Notebook"
+            style={animation}
+          />
+
+          <Box display="flex" w={"50%"} flexDirection="column"  alignItems="center" m="xl" p="1em">
+            {/* Tagline */}
+
+              <button style={{width:"100%", fontSize:"20px", backgroundColor:"#ccd5ae", opacity: btnOpacity, height: "5em", transition:"opacity 2s"}} 
+              id={"btn-signin"}
+              onClick={() => navigate("/signin")}
+              >
+                Start writing now
+              </button>
+              <br />
+
+           
+          </Box>
+
+          
+        </Flex>
       </section>
+
+
 
       {/* Define the keyframes for the spin animation */}
       <style>
@@ -50,6 +95,10 @@ function Home() {
           @keyframes spin {
             0% { transform: rotate(0deg); }
             100% { transform: rotate(360deg); }
+          }
+
+          #btn-signin:hover {
+            border:3px solid #000000;
           }
         `}
       </style>
